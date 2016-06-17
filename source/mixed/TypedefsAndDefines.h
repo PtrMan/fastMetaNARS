ac6f0d49ee9d3654cf72a9e519c895f8ab620b88
PtrMan <robertw89@googlemail.com>
2016-06-17 18:01:39 +0200

    {nars}[C++] removed TermIdType from types

--- source/mixed/TypedefsAndDefines.h
+++ source/mixed/TypedefsAndDefines.h
@@ -9,9 +9,3 @@ const bool CHECK = true;
 typedef uint32_t ConceptTermIndexType;
 typedef ConceptTermIndexType ConceptIndexType;
 typedef ConceptTermIndexType TermIndexType;
-
-// unique id for a term for the state of the reasoner
-typedef uint32_t TermIdType;
-
-// unique id for a term for the state of the reasoner
-typedef uint32_t TermIdType;
