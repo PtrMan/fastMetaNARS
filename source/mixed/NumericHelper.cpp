b77e77fa18a12741aa015fb3cf87455dd4fb9f85
PtrMan <robertw89@googlemail.com>
2016-06-20 15:27:03 +0200

    [C++]{nars} splitted NumericHelper into C++ and header datei

---
+++ source/mixed/NumericHelper.cpp
@@ -0,0 +1,19 @@
+#include "NumericHelper.h"
+
+unsigned integerLog(uint32_t v) {
+	const uint32_t b[] = { 0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000 };
+	const uint32_t S[] = { 1, 2, 4, 8, 16 };
+
+	unsigned r = 0; // result of log2(v) will go here
+	for (int i = 4; i >= 0; i--) {
+		if (v & b[i]) {
+			v >>= S[i];
+			r |= S[i];
+		}
+	}
+	return r;
+}
+
+bool isPowerOfTwo(uint32_t v) {
+	return v && !(v & (v - 1));
+}
