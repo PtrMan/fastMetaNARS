89402ea73ac434dc497900f3d0aa07a3cbe9e095
PtrMan <robertw89@googlemail.com>
2017-02-03 18:05:53 +0100

    [D] temporal linkage has to be stored between beliefs and not tasks

--- source/d/fastMetaNars/ClassicalBelief.d
+++ source/d/fastMetaNars/ClassicalBelief.d
@@ -5,4 +5,21 @@ import fastMetaNars.TermOrCompoundTermOrVariableReferer;
 struct ClassicalBelief {
 	// https://github.com/opennars/opennars/blob/1.6.5_devel17_TonyAnticipationStrategy/nars_core/nars/entity/Concept.java#L91
 	TermOrCompoundTermOrVariableReferer term;
+
+	// holds all temporal related information like for example
+	// * input chaining, how long is the temporal difference to the input tasks before it
+	static struct TemporalInformation {
+		static struct Chaining {
+			ClassicalBelief *beliefBefore;
+			uint deltaTime; // in system steps
+			bool withGap; // was there a gap between this and the previous task
+			              // has the AI forgotten a task between taskBefore and this
+			bool isImmediatePrecursor; // is taskBefore the task which was received directly before this task
+		}
+
+		Chaining[] chainingSkiplist; // NOTE< if an task is removed we have to maintain this list too >
+		                             // NOTE< currntly we maintain only a single linked list >
+	}
+
+	TemporalInformation temporalInformation;
 }
--- source/d/fastMetaNars/ClassicalTask.d
+++ source/d/fastMetaNars/ClassicalTask.d
@@ -11,21 +11,4 @@ struct ClassicalTask {
 	ClassicalSentence bestSolution; // for question and goal: best solution found so far
 
 	bool isInput; // is it an input task
-
-	// holds all temporal related information like for example
-	// * input chaining, ow long is the temporal difference to the input tasks before it
-	static struct TemporalInformation {
-		static struct Chaining {
-			ClassicalTask *taskBefore;
-			uint deltaTime; // in system steps
-			bool withGap; // was there a gap between this and the previous task
-			              // has the AI forgotten a task between taskBefore and this
-			bool isImmediatePrecursor; // is taskBefore the task which was received directly before this task
-		}
-
-		Chaining[] chainingSkiplist; // NOTE< if an task is removed we have to maintain this list too >
-		                             // NOTE< currntly we maintain only a single linked list >
-	}
-
-	TemporalInformation temporalInformation;
 }
