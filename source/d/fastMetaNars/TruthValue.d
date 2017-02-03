d6ebbcc307dbad6c5db8cf554b2864332d6e9e0f
PtrMan <robertw89@googlemail.com>
2017-02-03 15:59:29 +0100

    [D] forgot to add TruthValue

---
+++ source/d/fastMetaNars/TruthValue.d
@@ -0,0 +1,12 @@
+module fastMetaNars.TruthValue;
+
+// https://github.com/opennars/opennars/blob/1.6.5_devel17_TonyAnticipationStrategy/nars_core/nars/entity/TruthValue.java
+struct TruthValue {
+	static TruthValue make(float frequency, float confidence) {
+		TruthValue result;
+		result.frequency = frequency;
+		result.confidence = confidence;
+	}
+
+	float frequency, confidence;
+}
